add2d5447526affb751a02b0b5400d6ff01ef693
valter <valter@teste.com>
2024-08-22 02:15:11 -0300

    atualização

---
+++ Games Shop/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
