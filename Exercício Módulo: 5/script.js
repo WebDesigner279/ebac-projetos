eefe9467191739f6186b35f24a2d384a7bb73986
valter <valter@teste.com>
2024-09-11 15:09:04 -0300

    Exercício Módulo: 5 Concluído

---
+++ Exercício Módulo: 5/script.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+
+    });
+
+
+   
