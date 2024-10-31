75ed64d4f6e74c1337f40d61ca1384a8f2aa26b2
valter <valter@teste.com>
2024-10-31 18:28:19 -0300

    atualização gulp

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,44 +1,37 @@
 // Importação do pacote Gulp
 const gulp = require('gulp');
 
-// Importação composta dos pacotes Gulp e SASS
+// Importação do Gulp e SASS
 const sass = require('gulp-sass')(require('sass'));
 
 // Compilação do SASS
 function compilaSass() {
-  return gulp.src('./source/styles/*.scss')
-    .pipe(sass())
+  return gulp.src('./source/styles/main.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./build/styles'));
 }
 
-
-
-// função publica das tarefas exportadas
+// Função para tarefas em série
 function funcaoPadrao(callback) {
-  // setTimeout define o tempo de execução do bloco de código
   setTimeout(function() {
     console.log("Executando via Gulp");
     callback();
   }, 2000);
 }
 
-// função publica das tarefas exportadas
+// Função pública `dizOi` com chamada à função `dizTchau`
 function dizOi(callback) {
   console.log("Olá Gulp");
   dizTchau();
   callback();
 }
 
-// função privada das tarefas não exportadas
+// Função privada
 function dizTchau() {
-console.log("Tchau Gulp");
+  console.log("Tchau Gulp");
 }
 
-// Execução de tarefas em série
-exports.default = gulp.series(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
-
-// Execução de tarefas em paralelo
-exports.default = gulp.parallel(funcaoPadrao, dizOi);
+// Exportação das tarefas
+exports.default = gulp.series(funcaoPadrao, dizOi, compilaSass); // Opção em série
 exports.dizOi = dizOi;
 exports.sass = compilaSass;
