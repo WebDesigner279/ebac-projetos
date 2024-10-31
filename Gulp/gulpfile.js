550a8eef906d7c3d2236783de082b5fcce1adf5e
valter <valter@teste.com>
2024-10-31 17:07:21 -0300

    atualização gulp

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,7 +1,25 @@
+// Importação do pacote Gulp
+const gulp = require('gulp');
+
+// Importação composta dos pacotes Gulp e SASS
+const sass = require('gulp-sass')(require('sass'));
+
+// Compilação do SASS
+function compilaSass() {
+  return gulp.src('./source/styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/styles'));
+}
+
+
+
 // função publica das tarefas exportadas
 function funcaoPadrao(callback) {
-  console.log("Executando via Gulp");
-  callback();
+  // setTimeout define o tempo de execução do bloco de código
+  setTimeout(function() {
+    console.log("Executando via Gulp");
+    callback();
+  }, 2000);
 }
 
 // função publica das tarefas exportadas
@@ -16,5 +34,11 @@ function dizTchau() {
 console.log("Tchau Gulp");
 }
 
-exports.default = funcaoPadrao;
+// Execução de tarefas em série
+exports.default = gulp.series(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
+
+// Execução de tarefas em paralelo
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
+exports.dizOi = dizOi;
+exports.sass = compilaSass;
