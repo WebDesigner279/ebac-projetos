03488151a46e0d5bdd4b70ae134336253df1eeb9
Valter Vieira Gomes Junior <64391272+WebDesigner279@users.noreply.github.com>
2024-11-11 17:23:08 +0000

    Observe mudanças com o Grunt

---
+++ Video_Aula_Modulo_GRUNT_JS/Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        files: {
+          'dev/styles/main.css': 'src/styles/main.less'
+        }
+      },
+      production: {
+        options: {
+          compress: true,
+        },
+        files: {
+          'dist/styles/main.min.css': 'src/styles/main.less'
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: ['src/styles/**/*.less'],
+        tasks: ['less:development']
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['less:production']);
+}
